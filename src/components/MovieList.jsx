a91e822dfe4839260d00b2d10d0062aa59d2c725
AlineCarolina <eualine2017@gmail.com>
2021-09-01 20:40:45 -0300

    cria MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
 // implement MovieList component here
+import React from 'react';
+import '../App.css';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.promp;
+    return (
+      <div>
+        { movies }
+      </div>);
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+}.isRequired;
+
+export default MovieList;
