df57b13b6ba5bbbbb10e3b18a98e465e49b895d8
Aline <eualine2017@gmail.com>
2022-08-03 15:02:27 -0300

    feat: refactoring header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
-// implement Header component here
 import React from 'react';
-import '../App.css';
+import '../styles/Header.css';
 
 class Header extends React.Component {
   render() {
-    return <header><h1>Movie Cards Library</h1></header>;
+    return (
+      <header>
+        <h1>
+          Movie Cards Library
+        </h1>
+      </header>
+    )
   }
 }
 
